43c24596741ffe979af668483fde8dc286578dfe
ksuigyama <k.sugiyama@apassist.com>
2019-09-14 17:37:18 +0900

    edit main.js

--- main.js
+++ main.js
@@ -3,26 +3,31 @@
  *   - getElementByIdを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
+const titleElement = document.getElementById('title');
+console.log(titleElement);
 
 /**
  * 課題2: id値が 'item-container' の要素を取得する
  *   - querySelectorを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
+const itemContainer = document.querySelector('#item-container');
+console.log(itemContainer);
 
 /**
  * 課題3: li要素を全て取得する
  *   - console.logで取得したDOMの中身を確認する
  */
-
+const itemContainerLists = itemContainer.querySelectorAll('li');
+console.log(itemContainerLists);
 
 /**
  * 課題4: class値が 'item' の要素を全て取得する
  *   - getElementsByClassNameを使う
  *   - console.logで取得したDOMの中身を確認する
  */
+const items1 = document.getElementsByClassName('item');
+console.log(items1);
 
 
 /**
@@ -30,4 +35,5 @@
  *   - querySelectorAllを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
+const items2 = document.querySelectorAll('.item');
+console.log(items2);
